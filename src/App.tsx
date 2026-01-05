a480f54b12cf16ec021d1f6c8a250bef9ccc8a06
thiagodnt <thiagodnt@hotmail.com>
2026-01-05 19:14:04 -0300

    feat: handle lista tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, type FormEvent } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [input, setInput] = useState<string>('');
+	const [tasks, setTasks] = useState<string[]>([]);
+	const [editTask, setEditTask] = useState({
+		enabled: false,
+		task: '',
+	});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function handleAdicionar(e: FormEvent): void {
+		e.preventDefault();
+
+		if (editTask.enabled) {
+			handleSalvarEditar();
+			return;
+		}
+
+		setTasks((t) => [...t, input]);
+		setInput('');
+	}
+
+	function handleEditar(item: string): void {
+		setInput(item);
+		setEditTask({
+			enabled: true,
+			task: item,
+		});
+	}
+
+	function handleSalvarEditar(): void {
+		const indexTask = tasks.findIndex((task) => task === editTask.task);
+		const allTasks = [...tasks];
+		allTasks[indexTask] = input;
+
+		setTasks(allTasks);
+		setEditTask({
+			enabled: false,
+			task: '',
+		});
+		setInput('');
+	}
+
+	function handleRemover(item: string): void {
+		setTasks((prevTasks) => prevTasks.filter((task) => task !== item));
+	}
+
+	return (
+		<div className="container">
+			<h1>Gerenciador de Tarefas</h1>
+			<form onSubmit={handleAdicionar}>
+				<label>
+					<input
+						type="text"
+						placeholder="Digite aqui a sua tarefa"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						required
+					/>
+				</label>
+				<button type="submit">
+					{editTask.enabled ? 'Editar Tarefa' : 'Adicionar Tarefa'}
+				</button>
+			</form>
+			{tasks.map((task, index) => (
+				<section key={index} className="task-list">
+					<span>{task}</span>
+					<div className="actions">
+						<button onClick={() => handleEditar(task)}>Editar</button>
+						<button onClick={() => handleRemover(task)}>Excluir</button>
+					</div>
+				</section>
+			))}
+		</div>
+	);
 }
 
-export default App
+export default App;
